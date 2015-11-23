1b0ecb10729a6fdb94a13b17eacf8eec9bb282fe
Marina Fomicheva <mari.fomicheva@gmail.com>
2015-11-23 11:42:39 +0000

    Removed unnecessary files

--- train_model.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TRAINING=$(dirname $0)/training_testing.py
-TRAINING_FILES_DIRECTORY=$1
-OUTPUT_DIRECTORY=$2
-
-for FILE in $TRAINING_FILES_DIRECTORY/* ; do
-    python $TRAINING --options train_model --output_directory $2 --training_set $FILE
-done
